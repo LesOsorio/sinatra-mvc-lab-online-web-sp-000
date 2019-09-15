1c3ad759549dc2cd2b8db4d59cdda2e6a5beab71
Leslie Osorio <lesosorio17@gmail.com>
2019-09-15 00:10:08 +0000

    Automatically backed up by Learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+class PigLatinizer
+  attr_accessor :word
+
+  def initalize(phrase=nil)
+    @phrase = phrase
+  end
+
+  def piglatinize(sentence)
+      sentence.split(" ").collect! {|word| piglatinize_word(word)}.join(' ')
+  end
+
+  def is_vowel?(word)
+    vowels = 'aeiouAEIOU'
+    vowels.include?(word[0])
+  end
+
+  def piglatinize_word(word)
+    vowels = 'aeiouAEIOU'
+    new_word = ""
+    if vowels.include?(word[0])
+      new_word = word.concat('way')
+    else
+      new_word = word.concat(word.slice!(/^[^aeiouAEIOU]*/i || "")) + 'ay'
+    end
+    new_word
+  end
+end
